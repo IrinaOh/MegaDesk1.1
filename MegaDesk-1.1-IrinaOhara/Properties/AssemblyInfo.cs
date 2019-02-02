95b41792d8fdc5e106d153f2e4c2be9bfaee8090
Irina Ohara <irina.v.ohara@gmail.com>
2019-02-02 15:57:27 -0700

    Initial Commit

--- MegaDesk-1.1-IrinaOhara/Properties/AssemblyInfo.cs
+++ MegaDesk-1.1-IrinaOhara/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MegaDesk-1-IrinaOhara")]
+[assembly: AssemblyTitle("MegaDesk-1.1-IrinaOhara")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MegaDesk-1-IrinaOhara")]
+[assembly: AssemblyProduct("MegaDesk-1.1-IrinaOhara")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
